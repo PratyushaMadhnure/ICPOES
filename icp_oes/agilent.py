a7b0f5cd0da2a52b7bf93abca09de3527160db21
Oscar Branson <oscarbranson@gmail.com>
2024-11-04 15:23:49 +0000

    fix special characters

--- icp_oes/agilent.py
+++ icp_oes/agilent.py
@@ -10,9 +10,12 @@ def load(f):
             dat = pd.read_excel(f, na_values='####', sheet_name='Intensity')
         case _:
             raise ValueError(f'File extension {file_ext} not supported - must be csv, xls, xlsx, or ods')
-
+        
     dat = dat.set_index('Solution Label')
     dat.drop('Rack:Tube', axis=1, inplace=True)
+    
+    # remove special characters
+    dat = dat.apply(lambda x: x.str.replace(' !', ''))
     dat = dat.apply(lambda x: pd.to_numeric(x, errors='coerce'))
     
     dat.columns = pd.MultiIndex.from_tuples([c[:3] for c in dat.columns.str.split(' ', expand=True)])
